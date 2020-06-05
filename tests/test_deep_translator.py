5c26975ece87f86a227ad3fbd009340ba52566f0
= <nidhalbacc@gmail.com>
2020-06-06 01:31:36 +0200

    added tests

--- tests/test_deep_translator.py
+++ tests/test_deep_translator.py
@@ -5,13 +5,15 @@
 import pytest
 from deep_translator import exceptions, GoogleTranslator
 
+
 @pytest.fixture
 def response():
     """Sample pytest fixture.
 
     See more at: http://doc.pytest.org/en/latest/fixture.html
     """
-    pass
+    translator = GoogleTranslator(target='en')
+    assert translator.translate(payload='좋은') == "good"
     with pytest.raises(exceptions.LanguageNotSupportedException):
         GoogleTranslator(source="", target="")
 
