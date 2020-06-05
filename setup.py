c2b5f71af4d7e74a03b30a66dd0cc37f6af67d1a
= <nidhalbacc@gmail.com>
2020-06-05 23:42:34 +0200

    added requirements

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['requests', 'bs4']
+
+with open('requirements.txt') as reqs:
+    requirements = reqs.read()
 
 setup_requirements = ['pytest-runner', ]
 
