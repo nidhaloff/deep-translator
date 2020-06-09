0b096c9264e491f101a27ce8e1021e560717f6ac
= <nidhalbacc@gmail.com>
2020-06-09 17:24:44 +0200

    fixed imports

--- deep_translator/__init__.py
+++ deep_translator/__init__.py
@@ -1,8 +1,10 @@
 """Top-level package for deep_translator."""
-from .deep_translator import GoogleTranslator
+from google_trans import GoogleTranslator
+from pons import PonsTranslator
+# import exceptions
 
 __author__ = """Nidhal Baccouri"""
 __email__ = 'nidhalbacc@gmail.com'
 __version__ = '0.1.2'
 
-__all__ = [GoogleTranslator]
+__all__ = [GoogleTranslator, PonsTranslator]
