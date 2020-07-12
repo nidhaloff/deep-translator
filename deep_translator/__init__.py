1bfafe328df0883013890ef68a799cb7bb19e902
= <nidhalbacc@gmail.com>
2020-07-12 14:35:30 +0200

    fixed autodetect bug

--- deep_translator/__init__.py
+++ deep_translator/__init__.py
@@ -8,7 +8,7 @@ from .mymemory import MyMemoryTranslator
 
 __author__ = """Nidhal Baccouri"""
 __email__ = 'nidhalbacc@gmail.com'
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 
 __all__ = [GoogleTranslator,
            PonsTranslator,
