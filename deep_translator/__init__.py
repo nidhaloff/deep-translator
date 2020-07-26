e07ed51158c86413414d2655e2dfe8a35060fd2a
= <nidhalbacc@gmail.com>
2020-07-26 18:23:02 +0200

    solved the certificate warning

--- deep_translator/__init__.py
+++ deep_translator/__init__.py
@@ -8,7 +8,7 @@ from .mymemory import MyMemoryTranslator
 
 __author__ = """Nidhal Baccouri"""
 __email__ = 'nidhalbacc@gmail.com'
-__version__ = '1.0.6'
+__version__ = '1.0.7'
 
 __all__ = [GoogleTranslator,
            PonsTranslator,
--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nidhaloff/deep_translator',
-    version='1.0.6',
+    version='1.0.7',
     zip_safe=False,
 )
