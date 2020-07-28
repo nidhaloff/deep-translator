426be436fbbfbe55545c86955d11ddfb82657e0e
= <nidhalbacc@gmail.com>
2020-07-28 22:49:13 +0200

    fixed version conflict

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nidhaloff/deep_translator',
-    version='1.0.8',
+    version='1.0.9',
     zip_safe=False,
 )
