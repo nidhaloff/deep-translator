cd83034584ba6bf206f1404b9caac0a4728ae6da
= <nidhalbacc@gmail.com>
2020-08-09 14:42:17 +0200

    updated examples

--- examples/pons.py
+++ examples/pons.py
@@ -2,9 +2,6 @@
 from deep_translator import PonsTranslator
 
 
-res = PonsTranslator(source='de', target='en').translate('übersetzen', return_all=False)
+res = PonsTranslator(source='en', target='de').translate('good', return_all=False)
 
-
-e2a = PonsTranslator('ar', 'en').translate('آخُذ اَلْباص.', return_all=True)
-
-print(e2a)
+print(res)
