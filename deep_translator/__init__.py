5a870b6a4562a89b05feec3825e051952f5dc784
= <nidhalbacc@gmail.com>
2020-08-23 23:28:05 +0200

    updated docs and added examples for qcri

--- deep_translator/__init__.py
+++ deep_translator/__init__.py
@@ -5,17 +5,19 @@ from .pons import PonsTranslator
 from .linguee import LingueeTranslator
 from .mymemory import MyMemoryTranslator
 from .yandex import YandexTranslator
+from .qcri import QCRI
 from .detection import single_detection, batch_detection
 
 
 __author__ = """Nidhal Baccouri"""
 __email__ = 'nidhalbacc@gmail.com'
-__version__ = '1.2.3'
+__version__ = '1.2.4'
 
 __all__ = [GoogleTranslator,
            PonsTranslator,
            LingueeTranslator,
            MyMemoryTranslator,
            YandexTranslator,
+           QCRI,
            single_detection,
            batch_detection]
--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nidhaloff/deep_translator',
-    version='1.2.3',
+    version='1.2.4',
     zip_safe=False,
 )
