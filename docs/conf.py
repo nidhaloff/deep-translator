7cecd8060490189138a51a1a0e9e7a7c63bd197d
= <nidhalbacc@gmail.com>
2020-09-10 21:59:34 +0200

    set side bar to false

--- docs/conf.py
+++ docs/conf.py
@@ -91,10 +91,7 @@ html_theme = 'classic'
 # theme further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    "rightsidebar": "false",
-    "relbarbgcolor": "black"
-}
+# html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
