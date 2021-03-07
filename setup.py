aa7e335e8ed2b77eb999b5a592eb33a6b5abc7d7
nidhal baccouri <nidhalbacc@gmail.com>
2021-03-07 14:10:47 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -70,6 +70,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nidhaloff/deep_translator',
-    version='1.3.9',
+    version='1.4.0',
     zip_safe=False,
 )
