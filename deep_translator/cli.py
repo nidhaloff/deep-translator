b96c7cffe3638d0be5b3d3a268e8ceb2ba3c80c3
Bighnesh Sahoo <bighnesh2001@gmail.com>
2021-04-01 17:10:49 -0400

    addded cli functionality to return the languages supported by the chosen translator service

--- deep_translator/cli.py
+++ deep_translator/cli.py
@@ -30,6 +30,27 @@ def translate(args):
     print(" | Translation from {} to {} |".format(args.source, args.target))
     print("Translated text: \n {}".format(res))
 
+def return_supported_languages(args):
+    """
+    function used to return the languages supported by the translator service from the parsed terminal arguments
+    @param args: parsed terminal arguments
+    @return: None
+    """
+    if args.translator == 'google':
+        translator = GoogleTranslator
+    elif args.translator == 'pons':
+        translator = PonsTranslator
+    elif args.translator == 'linguee':
+        translator = LingueeTranslator
+    elif args.translator == 'mymemory':
+        translator = MyMemoryTranslator
+    else:
+        print("given translator is not supported. Please use a supported translator from the deep_translator tool")
+
+    translator_supported_languages = translator.get_supported_languages(as_dict=True)
+    print(f'Languages supported by \'{args.translator}\' are :\n')
+    print(translator_supported_languages)
+
 
 def main():
     """
@@ -38,13 +59,17 @@ def main():
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('--translator', '-trans',
-                        default='google', type=str, help="name of the translator you want to use")
-    parser.add_argument('--source', '-src', type=str, help="source language to translate from", required=True)
-    parser.add_argument('--target', '-tg', type=str, help="target language to translate to", required=True)
-    parser.add_argument('--text', '-txt', type=str, help="text you want to translate", required=True)
+                        default='google', type=str, help="name of the translator you want to use", required=True)
+    parser.add_argument('--source', '-src', type=str, help="source language to translate from")
+    parser.add_argument('--target', '-tg', type=str, help="target language to translate to")
+    parser.add_argument('--text', '-txt', type=str, help="text you want to translate")
+    parser.add_argument('--languages', '-lang',action='store_true', help="all the languages available with the translator. Run the command deep_translator -trans <translator service> -lang")
 
     args = parser.parse_args()
-    translate(args)
+    if args.languages:
+        return_supported_languages(args)
+    else:
+        translate(args)
     # sys.exit()
 
 
