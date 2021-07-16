588393ddcf08af6e69b3b1f369221648ec599fec
Chris <trenthemc@gmail.com>
2021-07-16 14:06:41 -0400

    fix to cli test

--- deep_translator/tests/test_cli.py
+++ deep_translator/tests/test_cli.py
@@ -3,14 +3,14 @@
 """Tests for the CLI interface."""
 
 from click.testing import CliRunner
-from deep_translator import cli
+from deep_translator import __main__
 
 def results_test():
     runner = CliRunner()
-    result = runner.invoke(cli.main, [ 'google', 'auto', 'en', '좋은'])
+    result = runner.invoke(__main__.main, [ 'google', 'auto', 'en', '좋은'])
     assert result.exit_code == 0
     assert result == 'good'
     
-    api_error = runner.invoke(cli.main, ['microsoft','auto','en','Zwei minimale Dellchen auf der Rückseite.'])
+    api_error = runner.invoke(__main__.main, ['microsoft','auto','en','Zwei minimale Dellchen auf der Rückseite.'])
     assert api_error.exit_code == 0
     assert api_error == "This translator requires an api key provided through --api-key"
