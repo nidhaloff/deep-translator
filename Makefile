7168aeeab2d5a13756afb1e65a9cca009f379f3c
nidhal baccouri <nidhalbacc@gmail.com>
2021-07-23 17:29:48 +0200

    fixed setup.cfg

--- Makefile
+++ Makefile
@@ -74,7 +74,7 @@ servedocs: docs ## compile the docs watching for changes
 	watchmedo shell-command -p '*.rst' -c '$(MAKE) -C docs html' -R -D .
 
 release: dist ## package and upload a release
-	twine upload dist/*
+	twine upload dist/* --verbose
 
 dist: clean ## builds source and wheel package
 	python3 setup.py sdist
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     zip_safe=False,
     setup_requires=['pytest-runner',],
-    install_requires=['requests','beautifulsoup4','click',],
+    install_requires=['requests','beautifulsoup4','click'],
     python_requires='>=3',
     tests_require=['pytest>=3',],
     include_package_data=True,
