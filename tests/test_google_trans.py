fd6b37428f0c39e70ae83dd675d7212836b42fa3
nidhal baccouri <nidhalbacc@gmail.com>
2021-11-18 22:42:08 +0100

    updated tests

--- tests/test_google_trans.py
+++ tests/test_google_trans.py
@@ -129,43 +129,19 @@ def google_translator():
     """
     return GoogleTranslator(target='en')
 
-
-def case_sensitivity_checks():
-    test_lang = 'Czech'
-    test_text = 'Hi, the sky is dark while the moon is white. Hurrah!! Denver is a city name in Colorado.'
-    translated_text = 'Ahoj, obloha je tmavá, zatímco měsíc je bílý. Hurá!! Denver je název města v Coloradu.'
-    test_cases = []
-    n = len(test_lang)
-    mx = 1 << n
-    test = test_lang.lower()
-    for i in range(mx):
-        combination = [k for k in test_lang]
-        for j in range(n):
-            if (((i >> j) & 1) == 1):
-                combination[j] = test_lang[j].upper()
-        temp = ""
-        for i in combination:
-            temp += i
-        test_cases.append(temp)
-    random_cases = 5
-    random_test_cases = random.sample(test_cases, random_cases)  # randomly choosing any five cases since list is in order of 2^n containing all cases
-    for case in random_test_cases:
-        assert GoogleTranslator(source='en', target=case).translate(test_text) == translated_text
-
 def multiple_names_lang_checks():
     assert GoogleTranslator(source='en', target='burMeSe').translate("Hello") == 'မင်္ဂလာပါ'
     assert GoogleTranslator(source='en', target='Oriya').translate("What's up?") == 'କଣ ଚାଲିଛି?'
     assert GoogleTranslator(source='en', target='kurManJi').translate("Nice is dice.") == 'Xweş xweş e.'
 
 def test_random_tranlations_cases_multiple_names():
-    random_sample_size = 5
+    random_sample_size = 2
     d = dict.fromkeys(list(TRANSLATED_RESULTS.keys()))
     random_lang_names = random.sample(d.keys(), random_sample_size)
     random_subset_dict = {k: TRANSLATED_RESULTS[k] for k in random_lang_names}
     for lang, translation in random_subset_dict.items():
         assert GoogleTranslator(source='en', target=lang).translate(test_text_standard) == translation
 
-    case_sensitivity_checks()
     multiple_names_lang_checks()
 
 def test_content(google_translator):
