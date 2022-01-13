c585ffe863e8ccc03a487c1fcbdf48ad1903fd8a
nidhal baccouri <nidhalbacc@gmail.com>
2022-01-13 18:35:30 +0100

    upadted version for patch

--- deep_translator/__init__.py
+++ deep_translator/__init__.py
@@ -14,7 +14,7 @@ from .libre import LibreTranslator
 
 __author__ = """Nidhal Baccouri"""
 __email__ = 'nidhalbacc@gmail.com'
-__version__ = '1.5.0'
+__version__ = '1.6.1'
 
 __all__ = [
     "GoogleTranslator",
