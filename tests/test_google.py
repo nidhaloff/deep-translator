f7b34083e59189d7750a2e609af08eda7ff7c116
Nidhal Baccouri <nidhalbacc@gmail.com>
2023-06-25 18:46:50 +0200

    removed tests for digit

--- tests/test_google.py
+++ tests/test_google.py
@@ -47,7 +47,6 @@ def test_empty_text(google_translator):
 
 def test_payload(google_translator):
     with pytest.raises(exceptions.NotValidPayload):
-        google_translator.translate(text="1234")
         google_translator.translate(text="{}")
         google_translator.translate(text="%@")
 
